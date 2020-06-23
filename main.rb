19d930dbab2abde0ef013edd6035b9168cb10a31
Y. Violet Guo <yueguo598@gmail.com>
2020-06-23 11:53:15 -0300

    game runs for 12 rounds, no scoring or choosing in the begining

--- main.rb
+++ main.rb
@@ -3,12 +3,13 @@ W = "W"
 BLANK = "?"
 
 class Board
-  attr_accessor :board_config
+  attr_accessor :board_config, :limit, :round
 
   def initialize
     @board_config = Array.new()
     @dim = 4 # 4 slots
     @limit = 12
+    @round = 0
     for i in 0...@limit
       @board_config.push(["_", "_", "_", "_"]) # 4 by 12, 2 for 
     end
@@ -33,6 +34,35 @@ class Board
     return feedback
   end
 
+  def print_board()
+    @board_config.each_with_index do |row, idx|
+      row.each_with_index do |elem, elem_idx|
+        print elem
+        if elem_idx == (@limit - 1)
+          print "\n"
+        else
+          print '|'
+        end
+      end
+    end
+  end
+  
+  def prompt()
+    guess = ""
+    loop do
+      guess = gets.strip.chars
+      if guess.length == 4
+        break
+      end
+    end # end of do while
+
+    @round +=1
+    @board_config[@round] = guess
+    peg = feedback_code(guess)
+    puts "Feedback"
+    puts peg.inspect
+  end
+
   private
   def generate_code
     # generate a 4 digit code peg
@@ -40,23 +70,24 @@ class Board
     code = []
     for i in 0...4
       seed = rand()
-      code.push((10 *seed / 2).floor)
+      code.push((10 *seed / 2).floor.to_s)
+      
     end
-    puts code.inspect
+    puts code.inspect # only for debugging
     return code
   end
 end
 
 
+
+
+
+
 def main
   board = Board.new()
-  # IO w/ terminal example move
-  guess = gets.strip.chars
-  puts guess
-  peg = board.feedback_code(guess)
-  puts "Feedback"
-  puts peg.inspect
-
+  while board.round < board.limit
+    board.prompt()
+  end
 end
 
 main
